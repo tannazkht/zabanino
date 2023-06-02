c4dd93ee2f9c81e18bf5b75f027231bd647b983a
Tannaz <tannaz.teymourian@gmail.com>
2023-06-02 17:52:58 +0330

    Changes to be committed:

---
+++ JavaScript/smooth-scroll-module.js
@@ -0,0 +1,13 @@
+// window.scrollTo({
+//     left: x,
+//     top: y,
+//     behavior: 'smooth'
+//   });
+
+const el = document.querySelector("#scroll-button");
+
+el.scrollTo({
+  left: x,
+  top: y,
+  behavior: 'smooth'
+});
